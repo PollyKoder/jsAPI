ce3b62fc1d59cb0470083ff4a39c004b65d0a43c
Polly <riviera.koder@gmail.com>
2024-07-02 12:27:35 +0200

    CatBreedListInJSfinished

--- script.js
+++ script.js
@@ -5,14 +5,32 @@ headlinePush.innerHTML = "API Cat breeds listed below";
 mainPage.appendChild(headlinePush);
 
 async function catBreeds() {
+    //Collecting the api link
+    //const li = document.createElement("li");
     const response = await fetch("https://catfact.ninja/breeds");
     console.log(response);
 
-    const breedCat = response.json()
-    //const paragraph = document.createElement("p");
-    breedCat.textContent(catBreeds.data.current_page);
-    mainPage.appendChild(breedCat);
+    //instructing js on what and how much information we want from the link, or the arrays available within the link
+    const breedCat = await response.json()
+    for ( let i = 0; i<25; i++) {
+        let text = breedCat.data[i].breed; 
+        //Making a p element to push/append the collected data into the html
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = text;
+        mainPage.appendChild(paragraph);
+        //data is now successfully pushed
+    }
+    console.log(breedCat);
+    
+    //breedCat = textContent(catBreeds.data.breed);
+    
     
 };
 
 catBreeds();
+
+
+const footer = document.getElementById("footerDOM");
+const footerText = document.createElement("h5");
+footerText.innerHTML = "Page made by Polly H. Riviera";
+footer.appendChild(footerText);
